bd005fdee5f48a73f72a3605f42aa2b994a759ee
Gabriel Cesar <gabrielcesar2@gmail.com>
2020-03-27 11:01:43 -0300

    Scrip refactory.

--- js/script.js
+++ js/script.js
@@ -5,86 +5,56 @@
 */
 
 var colors = [
-    '#c1e583', '#c6e779', '#cbe96f', '#d0eb65', '#d4ed5b',
-    '#d9ef51', '#def147', '#e3f33c', '#e7f532', '#ecf728',
-    '#f1f91e', '#f6fb14', '#fafd0a', '#ffff00', '#fff70b',
-    '#ffef16', '#ffe721', '#ffe02c', '#ffd837', '#ffd042',
-    '#ffc84d', '#ffc058', '#ffb863', '#ffb06e', '#ffa979',
-    '#ffa184', '#ff998f'
-];
+    '#ff998f', '#ffa184', '#ffa979', '#ffb06e', '#ffb863', 
+    '#ffc058', '#ffc84d', '#ffd042', '#ffd837', '#ffe02c', 
+    '#ffe721', '#ffef16', '#fff70b', '#ffff00', '#fafd0a', 
+    '#f6fb14', '#f1f91e', '#ecf728', '#e7f532', '#e3f33c', 
+    '#def147', '#d9ef51', '#d4ed5b', '#d0eb65', '#cbe96f', 
+    '#c6e779', '#c1e583'
+]
 
-function tooltip(event, label) {
+tooltip = (event, label) => {
     document.getElementById('arwen_tooltip').innerHTML = label
     document.getElementById('arwen_tooltip').style.display = 'block'
     document.getElementById('arwen_tooltip').style.top = event.clientY - 45 + 'px'
     document.getElementById('arwen_tooltip').style.left = event.clientX + 16 + 'px'
 }
 
-function tooltip_close() {
-    document.getElementById('arwen_tooltip').style.display = 'none'
-}
+tooltip_close = () => { document.getElementById('arwen_tooltip').style.display = 'none' }
 
-function chart_evolution_label(date, confirmed, death)
-{
+chart_evolution_label = (date, confirmed, death) => {
     document.getElementById('arwen_chart_evolution_label').innerHTML = date + ' &bull; Confirmados: <span class=\'arwen_color_orange arwen_font_size_10\'>' + confirmed + '</span> &bull; Mortes: <span class=\'arwen_color_red arwen_font_size_10\'>' + death + '</span>'
 }
 
-// state, code, suspect, confirmed, recovered, death
-var states = []
-var states_color = []
-
 fetch("https://gabrielcesar.github.io/covid-br/data/covid.json")
 .then(response => response.json())
-.then(function (data) {
-    for (var state = 0; state < data.length; state++) {
-        states.push({
-            'name': data[state]['state'],
-            'code': data[state]['code'],
-            'suspect': data[state]['suspect'],
-            'confirmed': data[state]['confirmed'],
-            'recovered': data[state]['recovered'],
-            'death': data[state]['death']
-        })
-    }
-
-    states.sort((a, b) => parseInt(a.confirmed) < parseInt(b.confirmed) ? 1 : -1);
-    
+.then((data) => {
+    data.sort((a, b) => parseInt(a.confirmed) < parseInt(b.confirmed) ? 1 : -1);
     document.getElementById('arwen_main').innerHTML = stateBorders
     
-    for (let stateIndex in states) {// = 0; state < states.length; state++) {
-        let state = states[stateIndex];
-        document.getElementById(state['name']).addEventListener("mousemove", function () {
-            tooltip(event, '<div class=\'arwen_icon_tooltip arwen_flag_' + state['code'] + '\'></div><span id=\'arwen_tooltip_label\'>' + state['name'] + "</span><br>Confirmados <span class='arwen_color_orange'>" + state['confirmed'] + "</span><br>Mortes <span class='arwen_color_red'>" + state['death'] + "</span>")
-            document.getElementById('arwen_tooltip_label').style.color = colors[state]
+    for (let stateIndex in data) 
+    {
+        let state = data[stateIndex];
+
+        document.getElementById(state['state']).addEventListener("mousemove", () => {
+            tooltip(event, '<div class=\'arwen_icon_tooltip arwen_flag_' + state['code'] + '\'></div><span id=\'arwen_tooltip_label\'>' + state['state'] + "</span><br>Confirmados <span class='arwen_color_orange'>" + state['confirmed'] + "</span><br>Mortes <span class='arwen_color_red'>" + state['death'] + "</span>")
+            document.getElementById('arwen_tooltip_label').style.color = colors[stateIndex]
         })
         
-        document.getElementById(state['name']).addEventListener("mouseout", function () {
-            tooltip_close()
-        })
-        document.getElementById('arwen_menu').innerHTML += '<div class=\'arwen_item\'><div class=\'arwen_icon arwen_flag_' + state['code'] + '\'></div>' + state['name'] + '<div class=\'arwen_amount arwen_death\'>' + state['death'] + '</div><div class=\'arwen_amount arwen_confirmed\'>' + state['confirmed'] + '</div></div>'
-        document.getElementById('arwen_menu_right').innerHTML += '<div class=\'arwen_item\'><div class=\'arwen_icon arwen_flag_' + state['code'] + '\'></div>' + state['name'] + '<div class=\'arwen_amount\'>' + state['death'] + '</div></div>'
-        states_color.push({
-            name: state['name'],
-            confirmed: parseInt(state['confirmed'])
-        })
+        document.getElementById(state['state']).addEventListener("mouseout", () => { tooltip_close() })
+        document.getElementById('arwen_menu').innerHTML += '<div class=\'arwen_item\'><div class=\'arwen_icon arwen_flag_' + state['code'] + '\'></div>' + state['state'] + '<div class=\'arwen_amount arwen_death\'>' + state['death'] + '</div><div class=\'arwen_amount arwen_confirmed\'>' + state['confirmed'] + '</div></div>'
+        document.getElementById('arwen_menu_right').innerHTML += '<div class=\'arwen_item\'><div class=\'arwen_icon arwen_flag_' + state['code'] + '\'></div>' + state['state'] + '<div class=\'arwen_amount\'>' + state['death'] + '</div></div>'
+        document.getElementById(state['state']).style.fill = colors[stateIndex]
     }
     
-    states_color.sort(function (a, b) {
-        return a.confirmed - b.confirmed
-    })
-    
-    for (let state_color = 0; state_color < states_color.length; state_color++) {
-        document.getElementById(states_color[state_color]['name']).style.fill = colors[state_color]
-    }
-    
-    document.getElementById('arwen_min').innerHTML = states_color[0]['confirmed']
-    document.getElementById('arwen_max').innerHTML = states_color[states_color.length - 1]['confirmed']
+    document.getElementById('arwen_min').innerHTML = data[data.length - 1]['confirmed']
+    document.getElementById('arwen_max').innerHTML = data[0]['confirmed']
 })
 
 // general
 fetch("https://raw.githubusercontent.com/gabrielcesar/covid-br/master/data/general.json")
 .then(response => response.json())
-.then(function (data) {
+.then((data) => {
     //document.getElementById('total_suspect').innerHTML = data['total_suspect']
     document.getElementById('total_confirmed').innerHTML = data['total_confirmed']
     //document.getElementById('total_recovered').innerHTML = data['total_recovered']
@@ -98,8 +68,7 @@ fetch("https://raw.githubusercontent.com/gabrielcesar/covid-br/master/data/gener
 // chart_evolution
 fetch("https://raw.githubusercontent.com/gabrielcesar/covid-br/master/data/evolution.json")
 .then(response => response.json())
-.then(function (data) {
-
+.then((data) => {
     document.getElementById('arwen_chart_evolution').innerHTML = chart_evolution_svg 
 
     var chart_width = 220
